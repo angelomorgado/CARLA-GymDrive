a0852f1e998b1dcb5714449605b23ede6d38e431
AmrMorgado <angelo.morgado@ubi.pt>
2024-05-28 17:22:05 +0100

    Updated helpful-scripts dir

--- helpful-scripts/test_torch_feature_extractor.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True, trust_repo=True)
-resnet50.eval().to(device)
-
-# Print the model architecture
-print(resnet50)
