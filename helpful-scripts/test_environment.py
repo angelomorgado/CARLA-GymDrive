8146f20864e96c886363024a6692e28e063a7e77
AmrMorgado <angelo.morgado@ubi.pt>
2024-06-19 14:28:24 +0100

    deleted useless scripts

--- helpful-scripts/test_environment.py
+++
@@ -1,8 +0,0 @@
-import gymnasium as gym
-from env.environment import CarlaEnv
-
-env = gym.make('carla-rl-gym-v0', time_limit=55, initialize_server=False, random_weather=True, synchronous_mode=False, continuous=False, show_sensor_data=False, has_traffic=False, verbose=False)
-
-print("Observation Space: ", env.observation_space)
-
-print("\nAction Space: ", env.action_space.n)
