5f8eed6dc4fe624313e8aad78391f15d10f66be3
AmrMorgado <angelo.morgado@ubi.pt>
2024-06-21 10:09:27 +0100

    updated dqn architecture to the recent configuration

--- agent/stablebaselines3_architectures.py
+++ agent/stablebaselines3_architectures.py
@@ -10,7 +10,69 @@ import cv2
 import numpy as np
 
 # ================== DQN ==================
-class CustomExtractor_DQN(BaseFeaturesExtractor):
+class CustomExtractor_DQN_End2end(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Dict):
+        # Compute the combined feature dimension
+        image_dim = 512  # Dimensionality of the EfficientNet features
+        rest_dim = 128   # Dimensionality of the rest features
+        features_dim = image_dim + rest_dim
+        
+        super().__init__(observation_space, features_dim=features_dim)
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Custom CNN for processing the RGB data
+        self.image_model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d(1)  # Global average pooling to get a fixed-size feature vector
+        ) 
+
+        # Define the neural network architecture for processing the rest of the input
+        self.rest_model = nn.Sequential(
+            nn.Linear(3, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU()
+        )
+
+    def forward(self, observations):
+        rgb_input, rest_input = self.process_observations(observations)
+        
+        image_features = self.image_model(rgb_input)
+        image_features = torch.flatten(image_features, 1)
+
+        rest_output = self.rest_model(rest_input)
+                
+        if len(rest_output.shape) == 1:
+            rest_output = rest_output.unsqueeze(0)
+        
+        combined_features = torch.cat((image_features, rest_output), dim=1)
+        
+        return combined_features
+    
+    def process_observations(self, observations):
+        rgb_data = F.interpolate(observations['rgb_data'], size=(224, 224), mode='bilinear', align_corners=False)
+        rgb_data = rgb_data / 255.0  # Normalize the pixel values to be in the range [0, 1]
+
+        return (rgb_data.float().to(self.device), observations['rest'])
+
+class CustomExtractor_DQN_Modular(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.spaces.Dict, end_to_end: bool = False):
         # Compute the combined feature dimension
         image_dim = 1280  # Dimensionality of the EfficientNet features
@@ -22,63 +84,46 @@ class CustomExtractor_DQN(BaseFeaturesExtractor):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Load EfficientNet model from NVIDIA Torch Hub
-        self.efficientnet = None
-        if not end_to_end:
-            self.efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
-            self.efficientnet = nn.Sequential(*list(self.efficientnet.children())[:-1])
-            self.efficientnet.eval()
-            for param in self.efficientnet.parameters():
-                param.requires_grad = False  # Freeze the EfficientNet parameters
-        else:
-            self.efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=False)
-            self.efficientnet = nn.Sequential(*list(self.efficientnet.children())[:-1])
-            self.efficientnet.train()
+        self.efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
+        self.efficientnet = nn.Sequential(*list(self.efficientnet.children())[:-1])
+        self.efficientnet.eval()
+        # Freeze the EfficientNet parameters
+        for param in self.efficientnet.parameters():
+            param.requires_grad = False  
 
         # Add adaptive average pooling to reduce the spatial dimensions to 1x1
         self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
 
-        # Rest: 6 -> 256 (position and target_position)
+        # Define the neural network architecture for processing the rest of the input
+        # 3 -> 256
         self.rest_model = nn.Sequential(
-            nn.Linear(6, 256),
-        )
-
-        self.final_model = nn.Sequential(
-            nn.Linear(features_dim, 512),  # Combine image and rest features, output 512-dimensional vector
+            nn.Linear(3, 128),
             nn.ReLU(),
-            nn.Linear(512, 4)  # Discrete actions
+            nn.Linear(128, 256),
+            nn.ReLU()
         )
 
     def forward(self, observations):
         rgb_input, rest_input = self.process_observations(observations)
         
-        image_features = self.efficientnet(rgb_input)
-        image_features = self.global_avg_pool(image_features)
-        image_features = torch.flatten(image_features, 1)
-        
         rest_output = self.rest_model(rest_input)
-        
+                
         if len(rest_output.shape) == 1:
             rest_output = rest_output.unsqueeze(0)
-        
-        combined_features = torch.cat((image_features, rest_output), dim=1)
-        
+
+        combined_features = torch.cat((rgb_input, rest_output), dim=1)
         return combined_features
-        # return self.final_model(combined_features)
     
     def process_observations(self, observations):
         rgb_data = F.interpolate(observations['rgb_data'], size=(224, 224), mode='bilinear', align_corners=False)
-        # Normalize the pixel values to be in the range [0, 1]
-        rgb_data = cv2.normalize(rgb_data.cpu().numpy(), None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-        rgb_data = torch.from_numpy(rgb_data).float().to(self.device)
+        rgb_data = rgb_data / 255.0  # Normalize the pixel values to be in the range [0, 1]
 
-        position = observations['position'].squeeze()
-        target_position = observations['target_position'].squeeze()
-        if len(position.shape) == 1:
-            position = position.unsqueeze(0)
-            target_position = target_position.unsqueeze(0)
-        rest = torch.cat((position,target_position),dim=1).to(self.device)
-      
-        return (rgb_data, rest)
+        rgb_data = torch.from_numpy(rgb_data).float().to(self.device)
+        rgb_data = self.efficientnet(rgb_data)
+        rgb_data = self.global_avg_pool(rgb_data)
+        rgb_data = torch.flatten(rgb_data, 1).squeeze(0)
+
+        return (rgb_data, observations['rest'])
 
 # ================== PPO ==================
 class CustomExtractor_PPO_End2end(BaseFeaturesExtractor):
@@ -121,10 +166,6 @@ class CustomExtractor_PPO_End2end(BaseFeaturesExtractor):
             nn.Linear(64, 128),
             nn.ReLU()
         )
-
-        # Define the final fully connected layers for the policy head
-        self.policy_head_mean = nn.Linear(features_dim, self.action_dim)
-        self.policy_head_std = nn.Linear(features_dim, self.action_dim)
 
     def forward(self, observations):
         rgb_input, rest_input = self.process_observations(observations)
@@ -178,10 +219,6 @@ class CustomExtractor_PPO_Modular(BaseFeaturesExtractor):
             nn.Linear(128, 256),
             nn.ReLU()
         )
-
-        # Define the final fully connected layers for the policy head
-        self.policy_head_mean = nn.Linear(features_dim, self.action_dim)
-        self.policy_head_std = nn.Linear(features_dim, self.action_dim)
 
     def forward(self, observations):
         rgb_input, rest_input = self.process_observations(observations)
